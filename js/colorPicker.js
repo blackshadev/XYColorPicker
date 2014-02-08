53101b66bca386a9b7c1e79a46c0ab384160446f
Vincent Hagen <vincent.hagen@student.uva.nl>
2014-02-08 16:50:01 +0100

    Added more RGB color spaces

--- js/colorPicker.js
+++ js/colorPicker.js
@@ -12,6 +12,8 @@
     this.createImage();
 }
 XYPicker.prototype.createImage = function() {
+    this.setRGBSpace();
+
     var width = this.canvas.width;
     var height = this.canvas.height;
     var img = this.ctx.createImageData(width, height);
@@ -42,18 +44,15 @@ XYPicker.prototype.xyToRgb = function(x, y) {
     var z = 1.0 - x - y;
     
     var Y = 1.0;
-    var X = ( Y / y ) * x;
-    var Z = ( Y / y ) * z;
+    var X = y > 0 ? ( Y / y ) * x : 1.0;
+    var Z = y > 0 ? ( Y / y ) * z : 1.0;
 
-    var r =  X * 1.612 - Y * 0.203 - Z * 0.302;
-    var g = -X * 0.509 + Y * 1.412 + Z * 0.066;
-    var b =  X * 0.026 - Y * 0.072 + Z * 0.962;
+    var RGB = this.XYZtoRGB(X, Y, Z);
+    var r = RGB[0];
+    var g = RGB[1];
+    var b = RGB[2];
 
-    r = r <= 0.0031308 ? 12.92 * r :  (1.0 + 0.055) * Math.pow(r, (1.0 / 2.4)) - 0.055;
-    g = g <= 0.0031308 ? 12.92 * g : (1.0 + 0.055) * Math.pow(g, (1.0 / 2.4)) - 0.055;
-    b = b <= 0.0031308 ? 12.92 * b : (1.0 + 0.055) * Math.pow(b, (1.0 / 2.4)) - 0.055;
-
-    // Correct values if one is greater than 1
+    // Normalize
     var maxValue = Math.max(r, g, b);
     if (maxValue > 1) {
         r /= maxValue;
@@ -67,6 +66,46 @@ XYPicker.prototype.xyToRgb = function(x, y) {
     b = Math.max(b, 0);
 
     return [r * 255, g * 255, b * 255];
+};
+XYPicker.prototype.setRGBSpace = function(name) {
+    // some other RGB matrices
+    // var r =  X * 1.612 - Y * 0.203 - Z * 0.302;
+    // var g = -X * 0.509 + Y * 1.412 + Z * 0.066;
+    // var b =  X * 0.026 - Y * 0.072 + Z * 0.962;
+
+    
+    // var r =  0.8951    * X + 0.2664   * Y - 0.1614 * Z;
+    // var g = -0.7502   * X + 1.7135   * Y + 0.0367 * Z;
+    // var b =  0.0389 * X - 0.0685 * Y + 1.0296  * Z;
+
+    switch(name) {
+        
+        case "PAL": case "SECAM": case "sRGB":
+            this.XYZtoRGB = function(X, Y, Z) {
+                var r =  3.0629 * X - 1.3932 * Y - 0.4758 * Z;
+                var g = -0.9693 * X + 1.8760 * Y + 0.0416 * Z;
+                var b =  0.0679 * X - 0.2289 * Y + 1.0694 * Z;
+
+                r = r <= 0.018 ? 4.5 * r : (1.0 + 0.099) * Math.pow(r, (0.45)) - 0.099;
+                g = g <= 0.018 ? 4.5 * g : (1.0 + 0.099) * Math.pow(g, (0.45)) - 0.099;
+                b = b <= 0.018 ? 4.5 * b : (1.0 + 0.099) * Math.pow(b, (0.45)) - 0.099;
+                return [r, g, b];
+            }
+        break;
+        default:
+        case "wide gamut": 
+            this.XYZtoRGB = function(X, Y, Z) {
+                var r =  1.4625 * X - 0.1845 * Y - 0.2734 * Z;
+                var g = -0.5228 * X + 1.4479 * Y + 0.0681 * Z;
+                var b =  0.0346 * X - 0.0958 * Y + 1.2875 * Z;
+
+                r = r <= 0.0031308 ? 12.92 * r :  (1.0 + 0.055) * Math.pow(r, (1.0 / 2.4)) - 0.055;
+                g = g <= 0.0031308 ? 12.92 * g : (1.0 + 0.055) * Math.pow(g, (1.0 / 2.4)) - 0.055;
+                b = b <= 0.0031308 ? 12.92 * b : (1.0 + 0.055) * Math.pow(b, (1.0 / 2.4)) - 0.055;
+                return [r, g, b];
+            }
+            break;
+    }
 };
 XYPicker.prototype.doClick = function(ev) {
     var x = (ev.pageX - this.canvas.offsetLeft);
